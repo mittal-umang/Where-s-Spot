20531f92d31f3b8ae91d16c21acf93182bfad0fc
umang.mittal <Umang.Mittal@ibm.com>
2019-07-06 11:50:43 -0400

    re commiting

--- myproject/wherespot/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.template import loader
-
-
-def index(request):
-    template=loader.get_template('wherespot/index.html')
-    title = "Where's Spot?"
-    context={'title':title}
-    return HttpResponse(template.render(context,request))
