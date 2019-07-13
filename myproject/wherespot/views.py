df23b6956223d77633b3f064c466da0fb6eb4ce5
umang.mittal <Umang.Mittal@ibm.com>
2019-07-12 20:56:45 -0400

    Third Tab.

--- myproject/wherespot/views.py
+++ myproject/wherespot/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.core.files.storage import FileSystemStorage
-import boto3
 import os
+from google.cloud import storage
+
 
 
 def index(request):
@@ -27,11 +28,13 @@ def index(request):
     #         find_dog_name = fs.save(find_dog.name, find_dog)
     #         uploaded_file_url_find_dog = fs.url(find_dog_name)
     #
-    # if request.POST and request.FILES.get('lostDog',False):
-    #     lost_dog = request.FILES['lostDog']
-    #     if not fs.exists(lost_dog.name):
-    #         lost_dog_name = fs.save(lost_dog.name, lost_dog)
-    #         uploaded_file_url_lost_dog = fs.url(lost_dog_name)
+
+    if request.POST and request.FILES.get('lostDog', False):
+        lost_dog = request.FILES['lostDog']
+        if not fs.exists(lost_dog.name):
+            lost_dog_name = fs.save(lost_dog.name, lost_dog)
+            uploaded_file_url_lost_dog = fs.url(lost_dog_name)
+            to_cloud(url=uploaded_file_url_lost_dog)
 
     return HttpResponse(template.render(context, request))
 
@@ -46,4 +49,16 @@ def to_model(url, context):
     print("hello,world from to_model ", str(os.getcwd() + os.path.abspath(url)))
     context["popup_active"] = True
     return context
+
+
 # TODO Send image URL to image recognition code
+
+
+def to_cloud(url):
+    os.environ[
+        "GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/umang.mittal/Desktop/project/UI/Where-s-Spot/myproject/wherespot/credentials.json"
+    file_name = str(os.getcwd() + os.path.abspath(url))
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket('regal-subject-244800.appspot.com')
+    blob = bucket.blob(url)
+    blob.upload_from_filename(file_name)
